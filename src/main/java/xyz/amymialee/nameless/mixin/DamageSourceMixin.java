d4a063b939183b62dbdf0edcd970e3b38da223f0
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2024-01-18 16:58:40 +0000

    nameless

--- src/main/java/xyz/amymialee/nameless/mixin/DamageSourceMixin.java
+++ src/main/java/xyz/amymialee/nameless/mixin/DamageSourceMixin.java
@@ -7,16 +7,24 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DamageSource.class)
 public class DamageSourceMixin {
     @WrapOperation(method = "getDeathMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getDisplayName()Lnet/minecraft/text/Text;"))
     private Text nameless$removeName(Entity instance, Operation<Text> original) {
         if (instance instanceof LivingEntity living && living.hasStatusEffect(StatusEffects.INVISIBILITY)) {
-            return Text.translatable("nameless.somebody");
+            return Text.literal("Somebody");
+        }
+        return original.call(instance);
+    }
+
+    @WrapOperation(method = "getDeathMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getDisplayName()Lnet/minecraft/text/Text;"))
+    private Text nameless$removeName(@NotNull LivingEntity instance, Operation<Text> original) {
+        if (instance.hasStatusEffect(StatusEffects.INVISIBILITY)) {
+            return Text.literal("Somebody");
         }
         return original.call(instance);
     }
